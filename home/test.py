49fcd453a3a2ae7bdb285635790527f21ac903d4
shoaib <shoaibbilal101@gmail.com>
2023-02-04 16:47:29 +0500

    test case

--- home/test.py
+++ home/test.py
@@ -1,12 +1,40 @@
 from django.test import TestCase
-from .models import Author
+from .models import Author, Book
 
-class AuthorTestCase(TestCase):
+class AuthorModelTestCase(TestCase):
     def setUp(self):
-        Author.objects.create(nickname='shoaib', firstname='Muhammad',lastname='Shoaib',birth_date="2003-06-16")
-        
-    def test_my_model(self):
-        my_model = Author.objects.get(nickname='shoaib', firstname='Muhammad',lastname='Shoaib',birth_date="2003-06-16")
-        self.assertEqual(my_model.firstname, 'Muhammad')
+        self.author = Author.objects.create(
+            firstname='John',
+            lastname='Doe',
+            birth_date='1980-01-01'
+        )
+    
+    def test_author_creation(self):
+        self.assertTrue(isinstance(self.author, Author))
+        self.assertEqual(self.author.firstname, 'John')
+        self.assertEqual(self.author.lastname, 'Doe')
+        self.assertEqual(self.author.birth_date, '1980-01-01')
 
+class BookModelTestCase(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(
+            firstname='Jane',
+            lastname='Doe',
+            birth_date='1981-01-01'
+        )
+        self.book = Book.objects.create(
+            author=self.author,
+            title='My First Book',
+            category='Fiction',
+            published='2000-01-01',
+            price='19.99',
+            rating=4
+        )
 
+    def test_book_creation(self):
+        self.assertTrue(isinstance(self.book, Book))
+        self.assertEqual(self.book.title, 'My First Book')
+        self.assertEqual(self.book.category, 'Fiction')
+        self.assertEqual(self.book.published, '2000-01-01')
+        self.assertEqual(self.book.price, '19.99')
+        self.assertEqual(self.book.rating, 4)
